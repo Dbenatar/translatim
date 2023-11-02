70b6f34255a8d0b21fd7e6e4b5b381a2f489df13
Dom <dom.benatar@hotmail.com>
2023-11-02 11:45:36 +0000

    server built

--- server/server.js
+++ server/server.js
@@ -5,10 +5,23 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 const axios = require("axios");
-// add your endpoints here
 
 app.get("/", (_, response) =>
   response._construct.json("Roote route for Translatim")
 );
+
+app.get("/translate", async (request, response) => {
+  const { word, from, to } = request.query;
+
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  response.json(wrangledData);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
